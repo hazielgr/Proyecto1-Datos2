808845050df69510fa5e7d99cb72add4f7fcbc9c
Joseph Jimenez <josephdjz@hotmail.com>
2020-06-11 15:32:17 -0600

    Updating JSON for nodes

--- client/JSON/jsonSerialize.cpp
+++ client/JSON/jsonSerialize.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include "jsonSerialize.h"
+
 /**
  * @brief this methods creates VsPointer Objects from messages
  * @param DataType
@@ -7,30 +8,39 @@
  * @param ID
  * @param References
  * @return true if was ok creating the Object
- * @version 2.0
+ * @version 3.0
  */
 bool jsonSerialize::create_Object(std::string DataType, std::string Data, std::string ID, std::string References) {
     if(DataType=="string"){
-        VsPointer<string> myPtr = VsPointer<string>::New();
-        *myPtr=Data;
-        std::cout << myPtr.getData() << std::endl;
-        std::cout << myPtr.getType() << std::endl;
-        std::cout << "Created VSPointer<string>" << std::endl;
+        Nodo<string>* nodo;
+        int IDInt=std::stoi(ID);
+        int ReferencesInt=std::stoi(References);
+        nodo->setDataType(DataType);
+        //nodo->setData(DataString);
+        nodo->setID(IDInt);
+        nodo->setReferences(ReferencesInt);
+        std::cout << "Created Nodo<string>*" << std::endl;
     }else if(DataType=="int"){
-        VsPointer<int> myPtr = VsPointer<int>::New();
-        int dataInt=std::stoi( Data );
-        *myPtr=dataInt;
-        std::cout << myPtr.getData() << std::endl;
-        std::cout << myPtr.getType() << std::endl;
+        Nodo<int>* nodo;
+        int *DataInt= reinterpret_cast<int *>(std::stoi(Data));
+        int IDInt=std::stoi(ID);
+        int ReferencesInt=std::stoi(References);
+        nodo->setDataType(DataType);
+        nodo->setData(DataInt);
+        nodo->setID(IDInt);
+        nodo->setReferences(ReferencesInt);
         std::cout << "Creating VSPointer<int>" << std::endl;
     }else if(DataType=="bool"){
-        VsPointer<bool> myPtr = VsPointer<bool>::New();
+        Nodo<bool>* nodo;
         bool b;
         std::istringstream is(Data);
         is >> std::boolalpha >> b;
-        *myPtr=b;
-        std::cout << myPtr.getData() << std::endl;
-        std::cout << myPtr.getType() << std::endl;
+        int IDInt=std::stoi(ID);
+        int ReferencesInt=std::stoi(References);
+        nodo->setDataType(DataType);
+        nodo->setData(reinterpret_cast<bool *>(b));
+        nodo->setID(IDInt);
+        nodo->setReferences(ReferencesInt);
         std::cout << "Creating VSPointer<bool>" << std::endl;
     }else{
         return false;
@@ -67,18 +77,18 @@ bool jsonSerialize::deCode(std::string giveMeString) {
     return false;
 }
 /**
- *@brief this methods receive a single pointer and return  string JSON
- *@param pointer receive a VsPointer<String>
+ *@brief this methods receive a single node and return  string JSON
+ *@param pointer receive a Nodo<string>* nodo
  *@return output
- * @version 1.0
+ * @version 2.0
  */
-std::string jsonSerialize::enCode(VsPointer<string> pointer) {
+std::string jsonSerialize::enCode(Nodo<string>* nodo) {
     Json::Value save;
     Json::FastWriter fastWriter;
-    save["ID"]="ID";
-    save["References"]=4;
-    save["Data"]=pointer.getData();
-    save["DataType"]=pointer.getType();
+    save["ID"]=nodo->getID();
+    save["References"]=nodo->getReferences();
+    save["Data"]=nodo->getData();
+    save["DataType"]=nodo->getDataType();
 
     Json::StyledWriter styledWriter;
     std::cout << styledWriter.write(save);
@@ -87,18 +97,18 @@ std::string jsonSerialize::enCode(VsPointer<string> pointer) {
     return output;
 }
 /**
- *@brief this methods receive a single pointer and return  string JSON
- *@param pointer receive a VsPointer<int>
+ *@brief this methods receive a single node and return  string JSON
+ *@param pointer receive a Nodo<int>* nodo
  *@return output
- * @version 1.0
+ * @version 2.0
  */
-std::string jsonSerialize::enCode(VsPointer<int> pointer) {
+std::string jsonSerialize::enCode(Nodo<int>* nodo) {
     Json::Value save;
     Json::FastWriter fastWriter;
-    save["ID"]="ID";
-    save["References"]=3;
-    save["Data"]=pointer.getData();
-    save["DataType"]=pointer.getType();
+    save["ID"]=nodo->getID();
+    save["References"]=nodo->getReferences();
+    save["Data"]=nodo->getData();
+    save["DataType"]=nodo->getDataType();
     Json::StyledWriter styledWriter;
     std::cout << styledWriter.write(save);
     std::string output = fastWriter.write(save);
@@ -106,18 +116,18 @@ std::string jsonSerialize::enCode(VsPointer<int> pointer) {
     return output;
 }
 /**
- *@brief this methods receive a single pointer and return  string JSON
- *@param pointer receive a VsPointer<bool>
+ *@brief this methods receive a single node and return  string JSON
+ *@param pointer receive a Nodo<bool>* nodo)
  *@return output
- * @version 1.0
+ * @version 2.0
  */
-std::string jsonSerialize::enCode(VsPointer<bool> pointer) {
+std::string jsonSerialize::enCode(Nodo<bool>* nodo) {
     Json::Value save;
     Json::FastWriter fastWriter;
-    save["ID"]="ID";
-    save["References"]=0;
-    save["Data"]=pointer.getData();
-    save["DataType"]=pointer.getType();
+    save["ID"]=nodo->getID();
+    save["References"]=nodo->getReferences();
+    save["Data"]=nodo->getData();
+    save["DataType"]=nodo->getDataType();
     Json::StyledWriter styledWriter;
     std::cout << styledWriter.write(save);
     std::string output = fastWriter.write(save);
--- client/JSON/jsonSerialize.h
+++ client/JSON/jsonSerialize.h
@@ -2,8 +2,8 @@
 #define LABBING_JSONSERIALIZE_H
 #include <jsoncpp/json/value.h>
 #include <jsoncpp/json/json.h>
+#include "Nodo.h"
 #include <iostream>
-#include "../VsPointer.h"
 /**
  * @file jsonSerialize.h
  * @class jsonSerialize
@@ -15,9 +15,9 @@ class jsonSerialize{
 public:
     static bool create_Object(std::string DataType, std::string Data, std::string ID, std::string References);
     static bool deCode(std::string giveMeString);
-    static std::string enCode(VsPointer<string> pointer);
-    static std::string enCode(VsPointer<int> pointer);
-    static std::string enCode(VsPointer<bool> pointer);
+    static std::string enCode(Nodo<string>* nodo);
+    static std::string enCode(Nodo<int>* nodo);
+    static std::string enCode(Nodo<bool>* nodo);
 };
 
 #endif //LABBING_JSONSERIALIZE_H
